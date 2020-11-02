dff70b8406b9da0f5f52039f2179855231dc92de
Julien MALKA <julien.malka@epfl.ch>
2020-11-02 19:39:48 +0100

    Graph library added

--- graphs.py
+++ graphs.py
@@ -1,31 +1,64 @@
 class Graph:
-    def __init__():
+    def __init__(self):
         self.nodes = []
 
 
-    def add_node(n):
+    def add_node(self, n):
         for node in self.nodes:
             if node.label == n.label:
                 return
-        self.node.append(node)
+        self.nodes.append(n)
 
-    def find_node(label):
+    def find_node(self, label):
         for node in self.nodes:
             if node.label == label:
                 return node
 
 
-    def add_edge(label1, label2):
-        node1 = find_node(label1)
-        node2 = find_node(label2)
+    def add_edge(self, label1, label2):
+        node1 = self.find_node(label1)
+        node2 = self.find_node(label2)
+        node1.link_to.append(node2)
+        node2.linked_by.append(node1)
+
+    def find_roots(self):
+        roots = []
+        for node in self.nodes:
+            if node.linked_by == []:
+                roots.append(node)
+        return roots
+
+
+
+    def topological_sort(self):
+        start = self.find_roots()
+        sorted = []
+        while len(start)>0:
+            node = start[0]
+            start.remove(node)
+            sorted.append(node)
+            for linked_node in node.link_to:
+                linked_node.linked_by.remove(node)
+                if linked_node.linked_by == []:
+                    start.append(linked_node)
+            node.link_to = []
+
+
+        if len(sorted) == len(self.nodes):
+            return sorted
+        else:
+            return "ERREUR"
+
+
 
 
 
 
 
 class Node:
-    def __init__(label, linked_to, linked_by):
+    def __init__(self, label):
         self.label = label
         self.mark = 0
-        self.linked_to = linked_to
-        self.linked_by = linked_by
+        self.link_to = []
+        self.linked_by = []
+
